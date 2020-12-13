710fee15431ee28267c4520dae27e5f0bf572efa
Kalana Wijesekara <kalanaw@99x.lk>
2020-12-13 16:33:40 +0530

    feat(networking): add wip sample for product list retrieved via network

---
+++ Networking/AwesomeProject/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ActivityIndicator, FlatList, Text, View, Image } from 'react-native';
+
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://6wavrrn6r7.execute-api.us-west-2.amazonaws.com/products')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json)
+        setData(json.Items)
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <View style={{ flex: 1}}>
+              <Text>{item.name}</Text>
+              <Image
+                style={{width: 50,height: 50}}
+                source={{
+                  uri: item.imageUrl,
+                }}
+              />
+            </View>
+          )}
+        />
+      )}
+    </View>
+    </SafeAreaView>
+  );
+};
