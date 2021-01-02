5215ce43a7d5b419a8b70500c7fe16c031c51bc6
Kalana Wijesekara <kalanaw@99x.lk>
2021-01-02 18:26:10 +0530

    refact(general): step 4 remove inline styling

--- Networking/AwesomeProject/app/containers/ProductList/ProductListItem.js
+++ Networking/AwesomeProject/app/containers/ProductList/ProductListItem.js
@@ -1,22 +1,48 @@
 import React from "react";
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 const ProductListItem = ({item}) => {
   return (
-    <View style={{ flex: 1, flexDirection: 'row', padding: 6 }}>
+    <View style={styles.container}>
       <Image
-        style={{width: 50,height: 50, margin: 10}}
+        style={styles.image}
         source={{
           uri: item.imageUrl,
         }}
       />
-      <View style={{ flex: 1, padding: 12 }}>
-        <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 6 }}>{item.name}</Text>
-        <Text style={{ marginBottom: 6 }}>{item.description}</Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.textName}>{item.name}</Text>
+        <Text style={styles.textDescription}>{item.description}</Text>
         <Text>Price: {item.price} {item.currency}</Text>
       </View>
   </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 6,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    margin: 10,
+  },
+  infoContainer: {
+    flex: 1,
+    padding: 12,
+  },
+  textName: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 6,
+  },
+  textDescription: {
+    marginBottom: 6,
+  },
+});
+
 
 export default ProductListItem;
--- Networking/AwesomeProject/app/containers/ProductList/index.js
+++ Networking/AwesomeProject/app/containers/ProductList/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, ActivityIndicator, FlatList, Text, View, Image } from 'react-native';
+import { SafeAreaView, ActivityIndicator, FlatList, View } from 'react-native';
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
