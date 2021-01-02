a539a8c879dcd5887b78c8e21df055065f01de7f
Kalana Wijesekara <kalanaw@99x.lk>
2021-01-02 16:28:23 +0530

    refact(general): step 2 move product list to own container

--- Networking/AwesomeProject/App.js
+++ Networking/AwesomeProject/App.js
@@ -1,50 +1,10 @@
 // fetchProduts - Little styling for product list
 
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, ActivityIndicator, FlatList, Text, View, Image } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import ProductList from "./app/containers/ProductList";
 
 export default App = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('https://6wavrrn6r7.execute-api.us-west-2.amazonaws.com/products')
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json)
-        setData(json.Items)
-      })
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
-
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-    <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          ItemSeparatorComponent={() => <View style={{ height: 1, backgroundColor: '#DDD' }} />}
-          renderItem={({ item }) => (
-            <View style={{ flex: 1, flexDirection: 'row', padding: 6 }}>
-              <Image
-                style={{width: 50,height: 50, margin: 10}}
-                source={{
-                  uri: item.imageUrl,
-                }}
-              />
-              <View style={{ flex: 1, padding: 12 }}>
-                <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 6 }}>{item.name}</Text>
-                <Text style={{ marginBottom: 6 }}>{item.description}</Text>
-                <Text>Price: {item.price} {item.currency}</Text>
-              </View>
-              
-            </View>
-          )}
-        />
-      )}
-    </View>
-    </SafeAreaView>
+      <ProductList />
   );
 };
---
+++ Networking/AwesomeProject/app/containers/ProductList/index.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ActivityIndicator, FlatList, Text, View, Image } from 'react-native';
+
+const ProductList = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://6wavrrn6r7.execute-api.us-west-2.amazonaws.com/products')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json)
+        setData(json.Items)
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          ItemSeparatorComponent={() => <View style={{ height: 1, backgroundColor: '#DDD' }} />}
+          renderItem={({ item }) => (
+            <View style={{ flex: 1, flexDirection: 'row', padding: 6 }}>
+              <Image
+                style={{width: 50,height: 50, margin: 10}}
+                source={{
+                  uri: item.imageUrl,
+                }}
+              />
+              <View style={{ flex: 1, padding: 12 }}>
+                <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 6 }}>{item.name}</Text>
+                <Text style={{ marginBottom: 6 }}>{item.description}</Text>
+                <Text>Price: {item.price} {item.currency}</Text>
+              </View>
+              
+            </View>
+          )}
+        />
+      )}
+    </View>
+    </SafeAreaView>
+  );
+};
+
+export default ProductList;
